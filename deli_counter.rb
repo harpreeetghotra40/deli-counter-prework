ca9824c91e4b218137da9392ac6f23ecdd08be18
HARPREET GHOTRA <harpreetghotra40@icloud.com>
2019-04-15 21:46:39 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,5 +15,15 @@ def line(katz_deli)
 end
 
 def take_a_number(katz_deli , name)
-  katz_deli.push
+  katz_deli.push(name)
+  number = katz_deli.size + 1
+  puts "Welcome, #{name}, You are number #{number} in line."
+end
+
+def now_serving(katz_deli)
+  if katz_deli.size == 0
+    puts "There is nobody waiting to be server!"
+  end
+  name = katz_deli.shift
+  puts "Currently severing #{name}."
 end
