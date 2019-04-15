4a25c4e4a6cdaae050f181c71ad916fd226a1f28
HARPREET GHOTRA <harpreetghotra40@icloud.com>
2019-04-15 21:37:42 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,14 @@
 # Write your code here.
+katz_deli = []
+
+def line(katz_deli)
+  if katz_deli.size == 0
+    return "The line is currently empty."
+  end
+  lineString = "The line is currently: "
+  iteration = 1
+  katz_deli.each do |name|
+    lineString += " #{iteration}. #{name}"
+    iteration += 1
+  end
+end
