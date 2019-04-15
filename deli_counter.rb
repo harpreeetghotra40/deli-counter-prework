12dd182062fdd663e3d5e5bca730117529aef550
HARPREET GHOTRA <harpreetghotra40@icloud.com>
2019-04-15 21:48:15 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,7 @@ katz_deli = []
 
 def line(katz_deli)
   if katz_deli.size == 0
-    return "The line is currently empty."
+    puts "The line is currently empty."
   end
   lineString = "The line is currently: "
   iteration = 1
