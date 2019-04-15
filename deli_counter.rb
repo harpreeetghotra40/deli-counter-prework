0cb8e333e7363ec92d7a3a8115244f661e57ffdc
HARPREET GHOTRA <harpreetghotra40@icloud.com>
2019-04-15 21:51:08 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -22,7 +22,7 @@ end
 
 def now_serving(katz_deli)
   if katz_deli.size == 0
-    puts "There is nobody waiting to be served!"
+    puts "There is nobody waiting to be server!"
   end
   name = katz_deli.shift
   puts "Currently serving #{name}."
