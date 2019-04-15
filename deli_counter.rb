23df516c99593068c3c7abd27f791e7cebedefb4
HARPREET GHOTRA <harpreetghotra40@icloud.com>
2019-04-15 21:57:43 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,6 @@ def line(katz_deli)
   if katz_deli.size == 0
     puts "The line is currently empty."
   end
-elsif katz_deli.size > 0
   lineString = "The line is currently:"
   iteration = 1
   katz_deli.each do |name|
@@ -13,8 +12,6 @@ elsif katz_deli.size > 0
     lineString += "#{iteration}. #{name}"
     iteration += 1
   end
-  puts lineString
-end
   return lineString
 end
 
